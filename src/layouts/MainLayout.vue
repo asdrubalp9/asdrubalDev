33b571fd84e95f63f61c40987713aafc19884581
Asdrubal Nitro <asdrubalp9@gmail.com>
2022-08-23 20:41:26 -0400

    first commit

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,100 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header class="transparent">
+      <q-toolbar
+      >
+        <!--
+          <q-btn
+          flat
+          dense
+          round
+          icon="menu"
+          aria-label="Menu"
+          @click="toggleLeftDrawer"
+        />
+        -->
+
+        <q-toolbar-title class="primary-font text-h2 q-pt-md q-ml-xl text-weight-bold">
+          Drú
+        </q-toolbar-title>
+        <q-space />
+        <q-btn
+          v-for="(menu, k) in topNavMenu"
+          flat
+
+          class="q-ma-md primary-font topnavbutton"
+          :label="menu.label"
+          :icon="menu.icon"
+          :aria-label="menu.ariaLabel"
+          :key="k"
+        />
+      </q-toolbar>
+    </q-header>
+
+    <q-drawer
+      v-model="leftDrawerOpen"
+      bordered
+    >
+      <q-list>
+        <q-item-label
+          header
+        >
+        </q-item-label>
+      </q-list>
+    </q-drawer>
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+import { defineComponent, ref } from 'vue'
+
+export default defineComponent({
+  name: 'MainLayout',
+  setup () {
+    const leftDrawerOpen = ref(false)
+    const topNavMenu = ref([
+      {
+        label: "Sobre Drú",
+        // icon: "list",
+        to: "#dru",
+      },
+      {
+        label: "Portafolio",
+        // icon: "list",
+        to: "#portafolio",
+      },
+      {
+        label: "Mis Servicios",
+        // icon: "list",
+        to: "#servicios",
+      },
+      {
+        label: "Contáctame",
+        // icon: "list",
+        to: "#contactame",
+      },
+    ])
+    return {
+      topNavMenu,
+      leftDrawerOpen,
+      toggleLeftDrawer () {
+        leftDrawerOpen.value = !leftDrawerOpen.value
+      }
+    }
+  }
+})
+</script>
+<style>
+
+  .topnavbutton {
+    transition: all .3s;
+  }
+  .topnavbutton:hover{
+    color: var(--q-primary);
+    transition: all .3s;
+  }
+  
+</style>
---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,67 @@
+<template>
+  <q-page>
+    <div class="row window-height flex flex-center">
+      <div class="col-1 column "> 
+        <q-icon
+        name="fa-brands fa-twitter"
+        class="text-white q-ma-sm"
+        size="md"/>
+        <q-icon
+        name="fa-brands fa-facebook"
+        class="text-white q-ma-sm"
+        size="md"/>
+      </div>
+      <div class="col-6">
+        <h1 class="hero-text text-h4">
+          Hola, Soy
+          <span class="primary-font">
+            Asdrúbal Perez
+          </span>
+          <span>
+            Desarrollador creativo
+          </span>
+        </h1>
+        <div class="row">
+          <div class="col-6">
+            <q-btn
+              label="Escribeme"
+              color="primary"
+              class="q-px-xl q-py-md text-weight-bold"
+              icon-right="fa-solid fa-paper-plane"
+            />
+          </div>
+          <div class="col-6">
+            <q-btn
+              flat
+              class="q-px-xl q-py-md text-weight-bold text-white"
+              label="Sobre mí"
+            />
+          </div>
+        </div>
+      </div>
+      <div class="col-5">
+
+      </div>
+    </div>
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'IndexPage'
+})
+</script>
+<style>
+  .hero-text {
+    color:white;
+  }
+  .hero-text span.primary-font {
+    color: var(--q-primary);
+    display:block!important;
+    line-height: 2em;
+    font-size:2em;
+    font-weight: bolder;
+  }
+</style>
