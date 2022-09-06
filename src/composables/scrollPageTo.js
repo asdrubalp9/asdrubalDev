94adb37892df6194420cb76c8d65030b61dd3c60
Asdrubal Nitro <asdrubalp9@gmail.com>
2022-09-06 20:46:56 -0300

    mend

--- src/composables/scrollPageTo.js
+++ src/composables/scrollPageTo.js
@@ -1,7 +1,7 @@
-// import gtm from "./../components/gtm.js";
+// import gtm from "src/components/gtm.js";
 export default function scrollPageTo(navEl) {
   if (navEl) {
-    //     gtm.logPage(navEl);
+    // gtm.logPage(navEl);
     let element = document.querySelector(`${navEl}`);
     if (element) element.scrollIntoView({ behavior: "smooth" });
   }
--- src/plugins/gtmPlugin.js
+++ src/plugins/gtmPlugin.js
@@ -2,6 +2,7 @@ import gtm from "src/components/gtm.js";
 
 export default ({ router }) => {
   router.afterEach((to, from) => {
+    console.log("aaaaaaaa");
     gtm.logPage(to.path);
   });
 };
